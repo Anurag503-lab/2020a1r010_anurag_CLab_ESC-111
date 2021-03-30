1890ebd02ed5386e4ad4ad49ba47c324afa7eb47
Rohit parhiar <thakuranurag1199@gmail.com>
2021-03-30 06:07:06 -0700

    Write a C program to perform addition

---
+++ lab01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("Please enter the first number\n");
+    scanf("%d",&num1);
+    printf("Please enter the second number\n");
+    scanf("%d",&num2);
+    
+    sum = num1 + num2;
+    printf("%d\n", sum);
+    return 0;
+
+}
