5ac3c9fa0a43a3f0efb54a1a02e3c7758a74c722
anurag <thakuranurag1199@gmail.com>
2021-03-30 10:33:22 -0700

    Write a C program to perform subtraction

--- lab02.c
+++ lab02.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2,sum;
+    int num1,num2,diff;
     /*declaration of variables*/
-    
+
     printf("Please enter the first number\n");
     scanf("%d",&num1);
     printf("Please enter the second number\n");
     scanf("%d",&num2);
     
-    sum = num1 + num2;
-    /*for calculating sum of two numbers*/
+    diff = num1-num2;
+    /*for calculating difference between two numbers*/
 
-    printf("sum=%d\n", sum);
+    printf("difference=%d",diff);
     return 0;
+
 
 }
