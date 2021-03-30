07c94c677bf8bd8e8a6783de3fd83a093c526ad6
anurag <thakuranurag1199@gmail.com>
2021-03-30 10:55:41 -0700

    Write a C program to perform addition, subtraction, multiplication and division.

--- lab01.c
+++ lab01.c
@@ -2,15 +2,15 @@
 int main()
 {
     int num1,num2,sum;
-    /*declaration of variables*/
-    
+    /*Declaration of variables*/
+
     printf("Please enter the first number\n");
     scanf("%d",&num1);
     printf("Please enter the second number\n");
     scanf("%d",&num2);
     
     sum = num1 + num2;
-    /*for calculating sum of two numbers*/
+    /*For calculating sum of two numbers*/
 
     printf("sum=%d\n", sum);
     return 0;
