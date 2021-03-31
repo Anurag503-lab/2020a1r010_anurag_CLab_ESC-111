c5502decb2c1565531eadff4085bc30476bb66fc
anurag <thakuranurag1199@gmail.com>
2021-03-31 04:52:19 -0700

    $ git commit -m

---
+++ lab03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+/*declaration of variables*/
+
+printf("Please enter the first number\n");
+scanf("%d",&a);
+
+printf("Please enter the second number\n");
+scanf("%d",&b);
+
+c = a*b;
+/*for multiplying two numbers*/
+
+printf("c = %d\n",c);
+return 0;
+
+}
